47addb31e2e10766fe2b975cf2219a1890065b40
dani-rybe <danilrybakov249@gmail.com>
2024-02-06 02:07:05 +0700

    changed EDITOR env var to nvim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,8 @@ if status is-interactive
     end
 end
 
+set EDITOR nvim
+
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/dani/.ghcup/bin $PATH # ghcup-env
 
 set LANG en_US.utf-8
