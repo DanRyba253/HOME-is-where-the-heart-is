89c31d55ab51537fd4691b62c19a99000dc39609
dani-rybe <danilrybakov249@gmail.com>
2024-02-06 01:23:17 +0700

    version 0.0

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -U fish_greeting
+    fish_vi_key_bindings
+
+    if test -z "$WAYLAND_DISPLAY"; and test "$XDG_VTNR" -eq 1
+        exec sway
+    end
+end
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/dani/.ghcup/bin $PATH # ghcup-env
+
+set LANG en_US.utf-8
