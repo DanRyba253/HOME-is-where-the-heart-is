2c717909235847f1e20744078e66b38159ab8113
dani-rybe <danilrybakov249@gmail.com>
2024-02-25 02:03:37 +0700

    remove intellij idea from package list

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -232,11 +232,11 @@ vim.keymap.set('n', '<leader><space>', require("buffer_manager.ui").toggle_quick
     { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
     -- You can pass additional configuration to telescope to change theme, layout, etc.
-    require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    require('telescope.builtin').find_files(require('telescope.themes').get_dropdown {
         winblend = 10,
         previewer = false,
     })
-end, { desc = '[/] Fuzzily search in current buffer' })
+end, { desc = '[/] Find files' })
 
 local function telescope_live_grep_open_files()
     require('telescope.builtin').live_grep {
