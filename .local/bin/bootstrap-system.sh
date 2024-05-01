9c2975c34f43e61fdd167ef4174b67b3c8a1a6cf
Dani Rybe <danilrybakov249@gmail.com>
2024-05-01 20:39:57 +0700

    added bootstrap-system.sh

--- .local/bin/bootstrap-system.sh
+++ .local/bin/bootstrap-system.sh
@@ -3,6 +3,14 @@
 # install native packages
 sudo pacman -S (cat .pkglist)
 
+# install yay
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ~
+rm -rf yay
+
 # install AUR packages
 yay -S --editmenu (sed '/yay/d' .aurpkglist)
 
